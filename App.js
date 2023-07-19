cf417cb2d8a0f6563ea4e1785bf62c568e3004fe
TitasR <titas7118@gmail.com>
2023-07-19 23:10:04 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import {Main} from "./Main";
+function App() {
+  return (
+<div className="App">
+  <Main />
+</div>
+  );
+}
+export default App;
---
+++ Main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+export const Main = () => {
+    return <div>
+        <button className="sidebar-toggle">
+            <span>View Details</span>
+</button>
+<nav className="nav"></nav>
+    <div className="logo">Patient Details</div>
+    <ul>
+        <li>
+            Age: 32  
+        </li>
+        <li>
+            Sex: Male
+        </li>
+        <li>
+            Blood Group:O +ve
+        </li>
+    </ul>
+
+    </div>;
+};
